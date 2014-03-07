fef93df0673e245e1cdb426eb275df2c0d6529fc
fernando <dinhog@hotmail.com>
2014-03-06 22:40:05 -0300

    .

--- routes/user.js
+++ routes/user.js
@@ -1,8 +1,19 @@
+var url = require('url'),
+	qs = require('querystring'),
+	mongoose = require('mongoose'),
+	tools = require('../lib/tools');
 
-/*
- * GET users listing.
- */
 
 exports.list = function(req, res){
   res.send("respond with a resource");
 };
+
+exports.login = function(req, res){
+
+	require('../models/usuarios');
+	req.session.lastView = req.host  + req.url;
+	
+	var mAnuncios = mongoose.model('anuncios'),
+
+
+}
