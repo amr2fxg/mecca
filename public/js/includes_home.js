55d64ce5f440824fce7e3addf72d048337cd7706
uessels <gherini@gmail.com>
2014-03-08 02:03:00 -0300

    alteracoes home

--- public/js/includes_home.js
+++ public/js/includes_home.js
@@ -1,7 +1,7 @@
 // ---------- distribute
 $.getScript("/js/utils/distribute.js", function() {
-	$("#categories").find("ul").distribute();
-	$("#last_searches").find("#results").distribute();
+	//$("#categories").find("ul").distribute();
+	//$("#last_searches").find("#results").distribute();
 });
 
 $('#last_product').click( function() {
