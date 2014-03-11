b4c244d12d1c9500fa4eda1c50495000974a4476
uessels <gherini@gmail.com>
2014-03-11 20:51:50 -0300

    menu
    drop notificações
    drop settings

--- public/js/index.js
+++ public/js/index.js
@@ -9,7 +9,7 @@ $.getScript("/js/utils/jquery.gritter.min.js", function() {
 
 	$('.menuBtnNotify').click( function() {
 		$.gritter.add({
-			title: 'TÃ­tulo da notificaÃ§Ã£o',
+			title: 'Título da notificação',
 			text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Delectus, ipsa, ducimus, tempora corporis tenetur a illo perspiciatis voluptate ex similique molestiae in obcaecati hic ut at. Aspernatur, quisquam quas ratione?'
 		});
 	});
@@ -21,15 +21,36 @@ $.getScript("/js/utils/CSSPlugin.min.js");
 $.getScript("/js/utils/EasePack.min.js");
 $.getScript("/js/utils/TweenLite.min.js", function() {
 
-	$('.menuBtnAcc').click( function() {
-		var menuAcc = $('#menuAcc');
+	$('.notifications').click( function() {
+		var menuNotifications = $('#menuNotifications');
+		var menuSettings = $('#menuSettings');
 
-		if( menuAcc.hasClass('closed') ) {
-			menuAcc.removeClass('closed').addClass('open');
-			TweenLite.to( menuAcc, 0.5, { opacity:1 } );
-		} else if( menuAcc.hasClass('open') ) {
-			menuAcc.removeClass('open').addClass('closed');
-			TweenLite.to( menuAcc, 0.5, { opacity:0 } );
+		if( menuNotifications.hasClass('closed') ) {
+			menuNotifications.removeClass('closed').addClass('open');
+			TweenLite.to( menuNotifications, 0.5, { opacity:1 } );
+			// close the other
+			menuSettings.removeClass('open').addClass('closed');
+			TweenLite.to( menuSettings, 0.5, { opacity:0 } );
+		} else if( menuNotifications.hasClass('open') ) {
+			menuNotifications.removeClass('open').addClass('closed');
+			TweenLite.to( menuNotifications, 0.5, { opacity:0 } );
+		}
+
+	});
+
+	$('.menuBtnSettings').click( function() {
+		var menuSettings = $('#menuSettings');
+		var menuNotifications = $('#menuNotifications');
+
+		if( menuSettings.hasClass('closed') ) {
+			menuSettings.removeClass('closed').addClass('open');
+			TweenLite.to( menuSettings, 0.5, { opacity:1 } );
+			// close the other
+			menuNotifications.removeClass('open').addClass('closed');
+			TweenLite.to( menuNotifications, 0.5, { opacity:0 } );
+		} else if( menuSettings.hasClass('open') ) {
+			menuSettings.removeClass('open').addClass('closed');
+			TweenLite.to( menuSettings, 0.5, { opacity:0 } );
 		}
 
 	});
